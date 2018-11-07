5a4ebeea5b5c21ea21e602059911a410d38f3eaf
“Rafael <rafael.freaner@gmail.com>
2018-11-07 16:50:08 -0500

    added initial state handling

--- bonobos-pan-zoom/src/App.js
+++ bonobos-pan-zoom/src/App.js
@@ -8,8 +8,30 @@ const small = 'https://bonobos-prod-s3.imgix.net/products/18158/original/SHIRT_S
 const medium = 'https://bonobos-prod-s3.imgix.net/products/18158/original/SHIRT_ShortSleeve_ZebraRun_JetBlack_hero1.jpg?h=1024&w=1024';
 const large = 'https://bonobos-prod-s3.imgix.net/products/18158/original/SHIRT_ShortSleeve_ZebraRun_JetBlack_hero1.jpg?h=1530&w=1530';
 
-class ImagePanAndZoom extends Component {
 
+/**
+ * ImagePanAndZoom component
+ *
+ * @returns {ReactElement} <div>
+ */
+class ImagePanAndZoom extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      zoomed: false
+    };
+
+    this.zoomIn = this.zoomIn.bind(this);
+    this.zoomOut = this.zoomOut.bind(this);
+  }
+
+
+  /**
+   * Returns responsive image with different image sources
+   *
+   * @returns {ReactElement} <div>
+   */
   generateResponsiveImage() {
     return (
       <picture className="responsive-image">
@@ -18,11 +40,34 @@ class ImagePanAndZoom extends Component {
     )
   }
 
+  zoomIn() {
+    this.setState({
+      zoomed: true
+    });
+  }
+
+  zoomOut() {
+    if (this.state.zoomed) {
+      this.setState({
+        zoomed: !this.state.zoomed
+      });
+    }
+  }
+
+  /**
+   * Renders zoom in and zoom out controls
+   *
+   * @returns {ReactElement} <div>
+   */
   renderControls() {
     return (
       <div className="controls">
-        <i className="image-zoom--in"/>
-        <i className="image-zoom--out"/>
+        <button onClick={this.zoomIn}className="control">
+          <i className="image-zoom--in"/>
+        </button>
+        <button  onClick={this.zoomOut} className="control">
+         <i className="image-zoom--out"/>
+        </button>
       </div>
     )
   }
